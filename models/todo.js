1868e127f7f80ddf24ff754023e2ed5bd3c4def8
NathanielJ14 <Nathanielalbert1202@gmail.com>
2023-11-10 15:49:25 -0500

    Clean up code

--- models/todo.js
+++ models/todo.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const TodoSchema = new Schema({
-    title: String,
-    description: String,
-    date: Date,
-    location: String
+    item: String
 });
 
 module.exports = mongoose.model('Todo', TodoSchema);
--- schemas.js
+++ schemas.js
@@ -1,8 +1,7 @@
 const Joi = require('joi');
 module.exports.todoSchema = Joi.object({
     Todo: Joi.object({
-        title: Joi.string().required(),
-        description: Joi.string().required(),
+        item: Joi.string().required()
     }).required()
 });
 
